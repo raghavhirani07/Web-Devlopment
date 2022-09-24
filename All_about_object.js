ffc1eba7d1318a5b70a58f916780350ebd3f6e8f
Raghav Hirani <90678701+raghavhirani07@users.noreply.github.com>
2022-09-24 21:33:55 +0800

    Add files via upload

---
+++ All_about_object.js
@@ -0,0 +1,11 @@
+const course = {
+    lecture: 10,
+    section: 3,
+    titel: 'javascript',
+    notes: {
+        introduction: "welcome to js cource"
+    },
+    enroll() {
+        console.log('you are succefullly enrolled');
+    }
+}
